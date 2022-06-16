7030d426ae0018481e5d1a992a8bc2be75ac2890
VilaCafe <107585508+VilaCafe@users.noreply.github.com>
2022-06-15 23:46:49 -0300

    Update cards.py

--- cards.py
+++ cards.py
@@ -2,26 +2,12 @@ import random
 
 
 class Cards:
-    """A deck of cards. Cards included are 1-13.
 
-    The value of the card is used for the main function of the game.
-   
-    Attributes:
-        value (int): The number of the card drawn.
-    """
 
     def __init__(self):
-        """Constructs instances of cards.
 
-        Args:
-            self (card): an instance of a card. 
-        """
         self.value = random.randint(1, 13)
 
     def getValue(self):
-        """Returns The value of the card.
-        
-        Args:
-            self (card): an instance of a card.
-        """
+
         return self.value
